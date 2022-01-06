8a21a1cbd82345b00128026b454ad74605e7669a
Toyo-Daichi <daichi.toyooka.gu@gmail.com>
2022-01-07 08:25:41 +0900

    add: select menu down

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -8,14 +8,14 @@ import { updateContent } from '../cache'
 import classes from './Input.module.scss' 
 
 // Main
-const Input = () => {
+const Input = ({handleNext}) => {
   const { state: { initialCache }, actions } = useStateMachine({ updateContent })
   const [site, setSite] = useState(initialCache.site)
   const [kind, setKind] = useState(initialCache.kind)
   const [labels, setLabels] = useState(initialCache.labels)
   //
   const Introduction = () => {
-    return <p>DX案件で調べた技術調査を記録として残しましょう。</p>
+    return <p>DX案件で調べた技術調査を記録として残しましょう！</p>
   }
   const handleSubmit = (props) => {
     actions.updateContent({site,labels})
@@ -32,19 +32,31 @@ const Input = () => {
             <FormControl fullWidth>
               <InputLabel id="site-kind">　サイト種類</InputLabel>
               <Select
-                labelId="demo-simple-select-label" id="demo-simple-select" value={kind} label="" variant='outlined'
+                labelId="site-kind" id="site-kind-id" value={kind} label="" variant='filled'
+                MenuProps={{
+                  anchorOrigin: {
+                    vertical: "bottom",
+                    horizontal: "left"
+                  },
+                  transformOrigin: {
+                    vertical: "top",
+                    horizontal: "left"
+                  },
+                  getContentAnchorEl: null,
+                }}
                 onChange={(event)=>setKind(event.target.value)}
               >
                 <MenuItem value={'Qiita'}>Qiita</MenuItem>
                 <MenuItem value={'Zenn'}>Zenn</MenuItem>
                 <MenuItem value={'Classmethod'}>Class method</MenuItem>
+                <MenuItem value={'公式Reference'}>AWS 公式Reference</MenuItem>
                 <MenuItem value={'その他'}>その他</MenuItem>
               </Select>
             </FormControl>
           </Grid>
           <Grid item xs={8}>
             <TextField
-              id="outlined-textarea" label="サイトURL" placeholder="https://" variant='outlined' fullWidth
+              id="outlined-textarea" label="サイトURL" placeholder="https://" variant='filled' fullWidth
               onChange={(event)=>setSite(event.target.value)}  
             />
           </Grid>
