8ebed2d2ef908a279fda37c95a233ea295aa62d1
Toyooka Daichi <daichi.toyooka.gu@gmail.com>
2022-01-10 16:00:44 +0900

    add: test card

--- frontend/src/components/Confirm.jsx
+++ frontend/src/components/Confirm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useStateMachine } from 'little-state-machine'
+import { Chrono} from 'react-chrono'
 import { Button, Grid } from '@material-ui/core'
 //
 import { updateContent } from '../cache'
@@ -12,6 +13,21 @@ const Confirm = (props) => {
   const [ kind ] = useState(initialCache.kind)
   const [ labels ] = useState(initialCache.labels)
   const [ comments ] = useState(initialCache.comments)
+  //
+  const timelines = [
+    {
+      title: "2022.1.10",
+      cardTitle: "Dunkirk",
+      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+    },
+    {
+      title: "May 1940",
+      cardTitle: "Dunkirk",
+      url: "http://www.history.com",
+      cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
+      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+    }
+  ]
   //
   const handleSubmit = (action) => {
     if (action === 'back'){
@@ -33,6 +49,7 @@ const Confirm = (props) => {
       <form onSubmit={(event)=>handleSubmit(event)} action='?'>
         <p>下記の内容でお待ちがいないですか？</p>
         <p>同カテゴリでは下記の内容が既に登録されています。似たような記事がある場合は、過去投稿にまとめて情報を整理しましょう！</p>
+        <Chrono items={timelines} />
         <p>サイトカテゴリ/サイト名：{kind}{site}</p>
         <p>技術カテゴリ：{labels}</p>
         <p>コメント：</p>
--- frontend/src/components/InputText.jsx
+++ frontend/src/components/InputText.jsx
@@ -14,16 +14,34 @@ const InputText = (props) => {
   const timelines = [
     {
       title: "2022.1.10",
-      cardTitle: "Dunkirk",
+      cardTitle: "https://localhost:3000",
+      cardSubtitle: "AWS, Python",
       cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
     },
     {
-      title: "May 1940",
-      cardTitle: "Dunkirk",
-      url: "http://www.history.com",
-      cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
+      title: "2022.1.10",
+      cardTitle: "https://localhost:3000",
+      cardSubtitle: "AWS, Python",
       cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-    }
+    },
+    {
+      title: "2022.1.10",
+      cardTitle: "https://localhost:3000",
+      cardSubtitle: "AWS, Python",
+      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+    },
+    {
+      title: "2022.1.10",
+      cardTitle: "https://localhost:3000",
+      cardSubtitle: "AWS, Python",
+      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+    },
+    {
+      title: "2022.1.10",
+      cardTitle: "https://localhost:3000",
+      cardSubtitle: "AWS, Python",
+      cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+    },
   ]
   //
   const handleSubmit = (action) => {
