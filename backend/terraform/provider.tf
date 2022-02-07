64ef8f9c61089c597610c21443b6487aed8363c6
Toyo-Daichi <daichi.toyooka.gu@gmail.com>
2022-02-07 17:49:55 +0900

    add: terraform cognito

--- backend/terraform/provider.tf
+++ backend/terraform/provider.tf
@@ -1,10 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
-  profile = "personal"
-  region = "ap-northeast-1"
+  profile = var.aws_profile
+  region = var.aws_region
 
   default_tags {
     tags = {
-      System = "pl"
+      system = var.sysname
+      env = var.env
     }
   }
 }
