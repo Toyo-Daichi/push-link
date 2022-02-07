64dad04c50281e934ba71b90870e70c794a66a4f
Toyo-Daichi <daichi.toyooka.gu@gmail.com>
2022-02-07 17:51:34 +0900

    add: basic commands

--- backend/cli/mk_cognito.sh
+++ backend/cli/mk_cognito.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 # Created on 2022.01.31 
-# Made use of Terraform
 
-#----------------------------------------------------------------------
-# +++ Set configure
-#----------------------------------------------------------------------
-sysname='pl'
-type='cognito'
-uses='pool'
-env='dev'
-#
-pool_name=${sysname}-${type}-${uses}-${env}
-
-#----------------------------------------------------------------------
-# +++ make Cognito Pool @Terraform
-#----------------------------------------------------------------------
+terraform init
+terraform plan
+terraform apply
 
 echo 'Normal END'
 
